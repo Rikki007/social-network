596f853843f9d9d8a9f9703deddd0a3b58a40e4e
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2024-08-16 10:45:11 +0300

    feat: add html

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,10 +1,30 @@
 import back from './back-pic.jpg';
+import ava from './avatar.png';
 
 const Main = () => {
   return (
-    <div className="back">
-      <img className="back__pic" src={ back } alt="picture"/>
-    </div>
+    <main>
+      <div className="back">
+        <img className="back__pic" src={back} alt="back-picture"/>
+      </div>
+      <div className="user-info">
+        <img className="user-info__avatar" src={ava} alt="avatar"/>
+        <h2 className="user-info__name">Jon Doe</h2>
+        <div className="user-infolist">
+          <p className="user-infolist__birth">22.04.1994</p>
+          <p className="user-infolist__city">Minsk</p>
+          <p className="user-infolist__education">BNTU</p>
+          <p className="user-infolist__more">
+            <a href="https://github.com/">github</a>
+          </p>
+        </div>
+      </div>
+      <div className="post-interface">
+        <h2 className="post-interface__Title">My posts</h2>
+        <textarea className="post-interface__text" placeholder="your news" rows="3" cols="55"></textarea>
+      </div>
+      <div></div>
+    </main>
   );
   
 }
--- my-app/src/sidebar.js
+++ my-app/src/sidebar.js
@@ -4,11 +4,11 @@ const Sidebar = () => {
   return (
     <aside className="sidebar">
       <ul className="sidebar-list">
-        <li className="sidebar-list__item"></li>
-        <li className="sidebar-list__item"></li>
-        <li className="sidebar-list__item"></li>
-        <li className="sidebar-list__item"></li>
-        <li className="sidebar-list__item"></li>
+        <li className="sidebar-list__item">Profile</li>
+        <li className="sidebar-list__item">Message</li>
+        <li className="sidebar-list__item">News</li>
+        <li className="sidebar-list__item">Music</li>
+        <li className="sidebar-list__item">Settings</li>
       </ul>
     </aside>
   );
