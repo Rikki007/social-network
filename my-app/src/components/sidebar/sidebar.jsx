14df8356abedbbd950c8110d31ee1bf691e7ad95
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2024-08-21 21:09:11 +0300

    feat: add icons and styles for sidebar

--- my-app/src/components/sidebar/sidebar.jsx
+++ my-app/src/components/sidebar/sidebar.jsx
@@ -1,14 +1,34 @@
 import obj from './sidebar.module.css';
+import profile from './sidebarAssets/user.svg';
+import message from './sidebarAssets/message.svg';
+import news from './sidebarAssets/news.svg';
+import music from './sidebarAssets/music.svg';
+import settings from './sidebarAssets/setting.svg';
 
 const Sidebar = () => {
   return (
     <aside className={obj.sidebar}>
       <ul className={obj.list}>
-        <li className={`${obj.item} ${obj.profile}`}>Profile</li>
-        <li className={`${obj.item} ${obj.message}`}>Message</li>
-        <li className={`${obj.item} ${obj.news}`}>News</li>
-        <li className={`${obj.item} ${obj.music}`}>Music</li>
-        <li className={`${obj.item} ${obj.settings}`}>Settings</li>
+        <li className={`${obj.item} ${obj.profile}`}>
+          <img src={profile} className={obj.icon} alt='icon'/>
+          <p>Profile</p>
+        </li>
+        <li className={`${obj.item} ${obj.message}`}>
+          <img src={message} className={obj.icon} alt='icon'/>
+          <p>Message</p>
+        </li>
+        <li className={`${obj.item} ${obj.news}`}>
+          <img src={news} className={obj.icon} alt='icon'/>
+          <p>News</p>
+        </li>
+        <li className={`${obj.item} ${obj.music}`}>
+          <img src={music} className={obj.icon} alt='icon'/>
+          <p>Music</p>
+        </li>
+        <li className={`${obj.item} ${obj.settings}`}>
+          <img src={settings} className={obj.icon} alt='icon'/>
+          <p>Settings</p>
+        </li>
       </ul>
     </aside>
   );
