cab32cff0d11fc401867d3839eab3f7a4652a980
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2024-08-22 20:48:43 +0300

    fix: change posts style

--- my-app/src/components/content/posts/posts.jsx
+++ my-app/src/components/content/posts/posts.jsx
@@ -4,10 +4,13 @@ import Post from './postSent/post';
 const Posts = () => {
   return (
     <div className={obj.interface}>
-        <h2 className={obj.title}>My posts</h2>
-        <textarea className={obj.text} placeholder="your news" rows="4" cols="55"></textarea>
-        <button className={obj.share} type="button">Send</button>
-        <Post/>
+      <h2 className={obj.title}>My posts</h2>
+      <textarea className={obj.text} placeholder="your news" rows="4"></textarea>
+      <div className={obj.buttons__container}>
+        <button className={`${obj.button} ${obj.share}`} type="button">Send</button>
+        <button className={`${obj.button} ${obj.remove}`} type="button">Remove</button>
+      </div>
+      <Post/>
     </div>
   );
   
