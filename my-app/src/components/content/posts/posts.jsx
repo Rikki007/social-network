a98981c19fe58a882cfc9e131e581400451e794c
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2024-08-26 19:27:48 +0300

    refactor: add data bll

--- my-app/src/components/content/posts/posts.jsx
+++ my-app/src/components/content/posts/posts.jsx
@@ -1,6 +1,12 @@
 import obj from './posts.module.css';
 import Post from './postSent/post';
 
+let postData = [
+  {id: 1, message: 'Hi, this is my first post!', likes: 13, dislikes: 4, share: 5},
+  {id: 1, message: 'what a wonderful day!', likes: 6, dislikes: 1, share: 8},
+  {id: 1, message: 'third post!', likes: 22, dislikes: 2, share: 1},
+];
+
 const Posts = () => {
   return (
     <div className={obj.interface}>
@@ -10,8 +16,13 @@ const Posts = () => {
         <button className={`${obj.button} ${obj.share}`} type="button">Send</button>
         <button className={`${obj.button} ${obj.remove}`} type="button">Remove</button>
       </div>
-      <Post message="Choonga changa" likes="13" dislikes="15" share="5" />
-      <Post message="Veselo jivet" likes="22" dislikes="11" share="8"/>
+
+      <Post message={postData[0].message} likes={postData[0].likes} dislikes={postData[0].dislikes} share={postData[0].share} />
+
+      <Post message={postData[1].message} likes={postData[1].likes} dislikes={postData[1].dislikes} share={postData[1].share} />
+
+      <Post message={postData[2].message} likes={postData[2].likes} dislikes={postData[2].dislikes} share={postData[2].share} />
+
     </div>
   );
   
--- my-app/src/components/dialogs/dialogs.jsx
+++ my-app/src/components/dialogs/dialogs.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 
 const DialogsItem = (props) => {
 
-  let path = '/dialogs/1' + props.id;
+  let path = '/dialogs/' + props.id;
   return (
 
     <li className={obj.item}>
@@ -15,6 +15,27 @@ const DialogsItem = (props) => {
 
   );
 }
+
+let dialogsData = [
+  {id: 1, name: 'Yana',},
+  {id: 2, name: 'Andrey',},
+  {id: 3, name: 'Vorona',},
+  {id: 4, name: 'Dmity',},
+  {id: 5, name: 'Inessa',},
+  {id: 6, name: 'Aleksandr',},
+  {id: 7, name: 'Voldemar',},
+  {id: 8, name: 'Ibragim',},
+];
+
+let messagesData = [
+  {id: 1, message: 'Hi'},
+  {id: 2, message: 'Hello'},
+  {id: 3, message: 'How are you?'},
+  {id: 4, message: 'So-so. You?'},
+  {id: 5, message: 'As well as you.'},
+];
+
+
 
 const Message = (props) => {
   return (
@@ -38,26 +59,25 @@ const Dialogs = (props) => {
       <div className={obj.chat}>
         <ul className={obj.list}>
 
-          <DialogsItem id='1' name='Yana' />
-          <DialogsItem id='2' name='Andrey' />
-          <DialogsItem id='3' name='Vorona' />
-          <DialogsItem id='4' name='Dmity' />
-          <DialogsItem id='5' name='Inessa' />
-          <DialogsItem id='6' name='Aleksandr' />
-          <DialogsItem id='7' name='Voldemar' />
-          <DialogsItem id='8' name='Ibragim' />
-          <DialogsItem id='9' name='Nurlan' />
+          <DialogsItem id={dialogsData[0].id} name={dialogsData[0].name} />
+          <DialogsItem id={dialogsData[1].id} name={dialogsData[1].name} />
+          <DialogsItem id={dialogsData[2].id} name={dialogsData[2].name} />
+          <DialogsItem id={dialogsData[3].id} name={dialogsData[3].name} />
+          <DialogsItem id={dialogsData[4].id} name={dialogsData[4].name} />
+          <DialogsItem id={dialogsData[5].id} name={dialogsData[5].name} />
+          <DialogsItem id={dialogsData[6].id} name={dialogsData[6].name} />
+          <DialogsItem id={dialogsData[7].id} name={dialogsData[7].name} />
 
         </ul>
       </div>
       <div className={obj.messages}>
 
-        <Message message='Hi'/>
-        <Message message='Hello'/>
-        <Message message='How are you?'/>
-        <Message message='So-so. You?'/>
-        <Message message='As well as you.'/>
-        
+        <Message message={messagesData[0].message}/>
+        <Message message={messagesData[1].message}/>
+        <Message message={messagesData[2].message}/>
+        <Message message={messagesData[3].message}/>
+        <Message message={messagesData[4].message}/>
+
       </div>
     </div>
   );
