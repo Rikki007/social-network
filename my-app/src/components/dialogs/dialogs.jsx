d95767ccd5ea42d4e73ae82b64d6b7d985802171
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2024-08-26 18:08:43 +0300

    refactor: add functions dialogsItem and Message and fix some css in dialogs window

--- my-app/src/components/dialogs/dialogs.jsx
+++ my-app/src/components/dialogs/dialogs.jsx
@@ -2,6 +2,35 @@ import obj from './dialogs.module.css';
 import avatar from './assets/avatar_icon.svg';
 import { NavLink } from 'react-router-dom';
 
+const DialogsItem = (props) => {
+
+  let path = '/dialogs/1' + props.id;
+  return (
+
+    <li className={obj.item}>
+      <NavLink to={path} className={navData => navData.isActive ? obj.choose : obj.link}>
+        {props.name}
+      </NavLink>
+    </li>
+
+  );
+}
+
+const Message = (props) => {
+  return (
+
+    <div className={obj.blok}>
+          <div className={obj.avatar}>
+            <img className={obj.picture} src={avatar} alt="icon" />         
+          </div>
+          <div className={obj.letter}>
+            <p className={obj.message}>{props.message}</p>
+          </div>
+        </div>
+
+  );
+}
+
 const Dialogs = (props) => {
   return (
     <div className={obj.wrapper}>
@@ -9,96 +38,25 @@ const Dialogs = (props) => {
       <div className={obj.chat}>
         <ul className={obj.list}>
 
-          <li className={`${obj.item} ${obj.choose}`}>
-            <NavLink to='/dialogs/1'>Sanya</NavLink>
-          </li>
+          <DialogsItem id='1' name='Yana' />
+          <DialogsItem id='2' name='Andrey' />
+          <DialogsItem id='3' name='Vorona' />
+          <DialogsItem id='4' name='Dmity' />
+          <DialogsItem id='5' name='Inessa' />
+          <DialogsItem id='6' name='Aleksandr' />
+          <DialogsItem id='7' name='Voldemar' />
+          <DialogsItem id='8' name='Ibragim' />
+          <DialogsItem id='9' name='Nurlan' />
 
-          <li className={obj.item}>
-            <NavLink to='/dialogs/2'>Vanya</NavLink>
-          </li>
-
-          <li className={obj.item}>
-            <NavLink to='/dialogs/3'>Lera</NavLink>
-          </li>
-
-          <li className={obj.item}>
-            <NavLink to='/dialogs/4'>Olga</NavLink>
-          </li>
-
-          <li className={obj.item}>
-            <NavLink to='/dialogs/'>Valera</NavLink>
-          </li>
-
-          <li className={obj.item}>
-            <NavLink to='/dialogs/'>Samuil</NavLink>
-          </li>
-
-          <li className={obj.item}>
-            <NavLink to='/dialogs/'>Voldemar</NavLink>
-          </li>
-
-          <li className={obj.item}>
-            <NavLink to='/dialogs/'>Sanya</NavLink>
-          </li>
-
-          <li className={obj.item}>
-            <NavLink to='/dialogs/'>Vanya</NavLink>
-          </li>
-
-          <li className={obj.item}>
-            <NavLink to='/dialogs/'>Lera</NavLink>
-          </li>
-
-          <li className={obj.item}>
-            <NavLink to='/dialogs/'>Olga</NavLink>
-          </li>
-
-          <li className={obj.item}>
-            <NavLink to='/dialogs/'>Valera</NavLink>
-          </li>
-
-          <li className={obj.item}>
-            <NavLink to='/dialogs/'>Samuil</NavLink>
-          </li>
-
-          <li className={obj.item}>
-            <NavLink to='/dialogs/'>Voldemar</NavLink>
-          </li>
-          
         </ul>
       </div>
       <div className={obj.messages}>
-        <div className={obj.blok}>
-          <div className={obj.avatar}>
-            <img className={obj.picture} src={avatar} alt="icon" />         
-          </div>
-          <div className={obj.letter}>
-            <p className={obj.message}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit sunt fuga, magni nostrum ipsum in ut dignissimos hic nesciunt soluta doloremque amet, iusto culpa ipsam nam ullam accusantium deserunt odit.
-            </p>
-          </div>
-        </div>
-        <div className={obj.blok}>
-          <div className={obj.avatar}>
-            <img className={obj.picture} src={avatar} alt="icon" />         
-          </div>
-          <div className={obj.letter}>
-            <p className={obj.message}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit sunt fuga, magni nostrum ipsum in ut dignissimos hic nesciunt soluta doloremque amet, iusto culpa ipsam nam ullam accusantium deserunt odit.
-            </p>
-          </div>
-        </div>
-        <div className={obj.blok}>
-          <div className={obj.avatar}>
-            <img className={obj.picture} src={avatar} alt="icon" />         
-          </div>
-          <div className={obj.letter}>
-            <p className={obj.message}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit sunt fuga, magni nostrum ipsum in ut dignissimos hic nesciunt soluta doloremque amet, iusto culpa ipsam nam ullam accusantium deserunt odit.
-            </p>
-          </div>
-        </div>
-        
+
+        <Message message='Hi'/>
+        <Message message='Hello'/>
+        <Message message='How are you?'/>
+        <Message message='So-so. You?'/>
+        <Message message='As well as you.'/>
         
       </div>
     </div>
