216bc274f184104fa02889ac5be6d03ad0a0e4a1
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2024-08-26 13:11:48 +0300

    fix: change sidebar links

--- my-app/src/components/sidebar/sidebar.jsx
+++ my-app/src/components/sidebar/sidebar.jsx
@@ -4,40 +4,46 @@ import message from './sidebarAssets/message.svg';
 import news from './sidebarAssets/news.svg';
 import music from './sidebarAssets/music.svg';
 import settings from './sidebarAssets/setting.svg';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <aside className={obj.sidebar}>
       <ul className={obj.list}>
         <li className={obj.item}>
-          <a className={obj.link} href='/profile'>
+          <NavLink className={navData => navData.isActive ? obj.active :
+            obj.link} to='/profile'>
             <img src={profile} className={obj.icon} alt='icon'/>
             <p>Profile</p>
-          </a>
+          </NavLink>
         </li>
         <li className={obj.item}>
-          <a className={obj.link} href='/dialogs'>
+          <NavLink className={navData => navData.isActive ? obj.active :
+            obj.link} to='/dialogs'>
             <img src={message} className={obj.icon} alt='icon'/>
             <p>Message</p>
-          </a>
+          </NavLink>
         </li>
         <li className={obj.item}>
-          <a className={obj.link} href='/news'>
+          <NavLink className={navData => navData.isActive ? obj.active :
+            obj.link} to='/news'>
             <img src={news} className={obj.icon} alt='icon'/>
             <p>News</p>   
-          </a>
+          </NavLink>
         </li>
         <li className={obj.item}>
-          <a className={obj.link} href='/music'>
+          <NavLink className={navData => navData.isActive ? obj.active :
+            obj.link} to='/music'>
             <img src={music} className={obj.icon} alt='icon'/>
             <p>Music</p>
-        </a>
+        </NavLink>
         </li>
         <li className={obj.item}>
-          <a className={obj.link} href='/settings'>
+          <NavLink className={navData => navData.isActive ? obj.active :
+            obj.link} to='/settings'>
             <img src={settings} className={obj.icon} alt='icon'/>
             <p>Settings</p>            
-          </a>
+          </NavLink>
         </li>
       </ul>
     </aside>
